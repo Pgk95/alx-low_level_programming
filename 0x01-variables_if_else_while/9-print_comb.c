97f3cfddc964159b8d65ed09b809912f2a369290
pgk95 <kalupgk@gmail.com>
2022-12-12 02:21:58 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,20 @@
  * main - prints all combination of single digits
  * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
-
-		putchar(n);
-		if (n != 57)
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
