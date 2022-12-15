7402c146adb5fad72c74731541729bd01d04ecda
pgk95 <kalupgk@gmail.com>
2022-12-15 03:18:50 -0800

    quick fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
@@ -22,11 +22,11 @@ int main(void)
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
