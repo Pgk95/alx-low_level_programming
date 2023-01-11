fe3ee18bf0644518efa2f8d3803b778e08b8192a
pgk95 <kalupgk@gmail.com>
2023-01-11 01:14:38 -0800

    quick fix on task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include "main.h"
 
 /*
- * main - print the name of the program
+ * main - print the number of the arguments passed to the program
  * @argc: count arguments
  * @argv: Arguments
  *
  * Return: Always 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 (void) argv; /*Ignore argv*/
-printf("%i\n", argc - 1);
+	printf("%i\n", argc - 1);
 
-return (0);
+	return (0);
 }
