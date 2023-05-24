f0a344d2f086b85a8d603631f1bcfdf0e56f3ebe
pgk95 <kalupgk@gmail.com>
2023-05-24 05:12:09 -0700

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""returns the permeter of the island."""
+
+def island_perimeter(grid):
+    
+    rows = len(grid)
+    colomuns = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(colomuns):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
